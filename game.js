c658142e50035b353ec4ff00565ed5e5085d66ae
SamuelRDev <the.sam.r@hotmail.fr>
2023-01-18 20:13:45 +0100

    ajout tours de jeu individuels + patch régles

--- game.js
+++ game.js
@@ -7,10 +7,15 @@ $(document).ready(function(){
     const textHeaderButton = $('header > div > h1').html()
     var imgDice = $('.render-view img')
     var imagesArray = ['./img/1.png', './img/2.png', './img/3.png', './img/4.png','./img/5.png', './img/6.png']
+    var numberThrow = 0
+    var diceValue = []
+    var finalDices = []
+    var numberOfTurn = 1
 
     body.addClass('step-1')
     header.css('height', '100vh')
     content.hide()
+
     headerButton.click(function(){
         body.removeClass('step-1').addClass('step-2')
         header.animate({height:100}, 400)
@@ -38,9 +43,6 @@ $(document).ready(function(){
     })
 
     //IN GAME FUNCTIONS  
-    var numberThrow = 0
-    var diceValue = []
-    var finalDices = []
     $('.button-start').click(function(){
         numberThrow+=1
         var numberOfDice = 6
@@ -63,14 +65,33 @@ $(document).ready(function(){
                     imgDice[i].parentNode.setAttribute('data-number', finalDices[i])
                 }
             }
-            finalDices = [1,2,3,4,5]
+            $('*').removeClass('disp-score')
             var throwScore = checkPoints(finalDices)
-            console.log(throwScore)
             var structured =  throwScore.structured
             var simple =  throwScore.simple
             animateStructured(structured)
             animateSimple(simple)
-            $("#chance").addClass('disp-score')
+            if(!$('#chance').hasClass('locked-score')){
+                $("#chance").addClass('disp-score')
+            }
+            
+            $('.disp-score').click(function(){
+                console.log("test")
+                if($(this).hasClass('disp-score')){
+                    $(this).toggleClass("locked-score")
+                }else{
+                    $(this).removeClass("locked-score")
+                }
+            })
+
+            $('.button-continue').click(function(){
+                if($('.locked-score').length == numberOfTurn){
+                    $('.only-once').remove()
+                    numberOfTurn++
+                    $('*').removeClass('disp-score')
+                    numberThrow = 0
+                }
+            })
         }
 
         if(numberThrow === 1 && body.hasClass('step-2')){
@@ -103,7 +124,6 @@ $(document).ready(function(){
         })
     })
 
-
     function diceThrow(min, max)
     {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -116,141 +136,168 @@ $(document).ready(function(){
             var dicePosition = parseInt($(this).attr('id') - 1)
             var diceNewValue = diceThrow(1,6)   
             diceValue.splice(dicePosition, 1, diceNewValue);
-            // console.log('position :' + dicePosition + ' devient ' + diceNewValue)
         });
         finalValues = diceValue
         return finalValues
     }
-})
-
-function countOccurences(arr) {
-    var counts = {};
-    for (var i = 1; i <= 6; i++) {
-        counts[i] = 0;
+    function countOccurences(arr) {
+        var counts = {};
+        for (var i = 1; i <= 6; i++) {
+            counts[i] = 0;
+        }
+        for (var i = 0; i < arr.length; i++) {
+            counts[arr[i]]++;
+        }
+        return counts;
     }
-    for (var i = 0; i < arr.length; i++) {
-        counts[arr[i]]++;
+    
+    function checkPoints(arr) {
+        var counts = countOccurences(arr)
+        var sorted = arr.slice()
+        sorted = sorted.sort()
+        var userScores = []
+        var scoreSimples = []
+        var structuredScores = []
+        var scoreChance = 0
+        for (var i = 1; i <= 6; i++) {
+            if (counts[i] == 2) {
+                for (var j = 1; j <= 6; j++) {
+                    if (i !== j && counts[j] == 3) {
+                        structuredScores.push('full')
+                    }
+                }
+            }else if (counts[i] === 3) {
+                structuredScores.push('brelan')
+            }else if(counts[i] === 4){
+                structuredScores.push('carre')
+            }else if(counts[i] === 5){
+                structuredScores.push('yahtzee')
+            }
+            scoreChance += counts[i] * i
+        }
+        for (var i = 0; i < sorted.length - 3; i++) {
+            if (sorted[i] + 1 == sorted[i+1] && sorted[i+1] + 1 == sorted[i+2] && sorted[i+2] + 1 == sorted[i+3]) {
+                structuredScores.push('petite suite')
+                break;
+            }
+        }
+        for (var i = 0; i < sorted.length - 4; i++) {
+            if (sorted[i] + 1 == sorted[i+1] && sorted[i+1] + 1 == sorted[i+2] && sorted[i+2] + 1 == sorted[i+3] && sorted[i+3] + 1 == sorted[i+4]) {
+                structuredScores.push('grande suite')
+                break;
+            }
+        }
+        scoreSimples.push(calculatePoints(arr))
+        userScores["structured"] = structuredScores
+        userScores["simple"] = scoreSimples
+        userScores["chance"] = scoreChance
+        return userScores
     }
-    return counts;
-}
-
-function checkPoints(arr) {
-    var counts = countOccurences(arr);
-    var userScores = []
-    var scoreSimples = []
-    var structuredScores = []
-    var scoreChance = 0
-    for (var i = 1; i <= 6; i++) {
-        if (counts[i] == 2) {
-            for (var j = 1; j <= 6; j++) {
-                if (i !== j && counts[j] == 3) {
-                    structuredScores.push('full')
+    
+    function calculatePoints(arr) {
+        var counts = countOccurences(arr);
+        var points = {};
+        for (var i = 1; i <= 6; i++) {
+            points[i] = i * counts[i];
+        }
+        return points;
+    }
+    
+    function animateStructured(arr){
+        for(let i = 0; i<arr.length; i++){
+            if(arr[i] === "brelan"){
+                if(!$('#brelan').hasClass('locked-score')){
+                    $('#brelan').addClass("disp-score")
+                }
+            }else{
+                if(arr[i] !== "full"){
+                    $('#brelan').removeClass("disp-score")
                 }
             }
-        }else if (counts[i] === 3) {
-            structuredScores.push('brelan')
-        }else if(counts[i] === 4){
-            structuredScores.push('carre')
-        }else if(counts[i] === 5){
-            structuredScores.push('yahtzee')
-        }
-        scoreChance += counts[i] * i
-    }
-    for (var i = 0; i < arr.length - 3; i++) {
-        if (arr[i] + 1 == arr[i+1] && arr[i+1] + 1 == arr[i+2] && arr[i+2] + 1 == arr[i+3]) {
-            structuredScores.push('petite suite')
-            break;
-        }
-    }
-    for (var i = 0; i < arr.length - 4; i++) {
-        if (arr[i] + 1 == arr[i+1] && arr[i+1] + 1 == arr[i+2] && arr[i+2] + 1 == arr[i+3] && arr[i+3] + 1 == arr[i+4]) {
-            structuredScores.push('grande suite')
-            break;
-        }
-    }
-    scoreSimples.push(calculatePoints(arr))
-    userScores["structured"] = structuredScores
-    userScores["simple"] = scoreSimples
-    userScores["chance"] = scoreChance
-    return userScores
-}
-
-function calculatePoints(arr) {
-    var counts = countOccurences(arr);
-    var points = {};
-    for (var i = 1; i <= 6; i++) {
-        points[i] = i * counts[i];
-    }
-    return points;
-}
-
-function animateStructured(arr){
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i] === "brelan"){
-            $('#brelan').addClass("disp-score")
-        }else{
-            $('#brelan').removeClass("disp-score")
-        }
-        if (arr[i] === "full"){
-            $('#full').addClass("disp-score")
-        }else{
-            $('#full').removeClass("disp-score")
-        }
-        if (arr[i] === "petite suite" || (arr[i] === "petite suite" && arr[i] === "grande suite")){
-            $('#petite-suite').addClass("disp-score")
-        }else{
-            $('#petite-suite').removeClass("disp-score")
-        }
-        if (arr[i] === "grande suite"){
-            $('#grande-suite').addClass("disp-score")
-        }else{
-            $('#grande-suite').removeClass("disp-score")
-        }
-        if (arr[i] === "carre"){
-            $('#carre').addClass("disp-score")
-        }else{
-            $('#carre').removeClass("disp-score")
-        }
-        if (arr[i] === "yahtzee"){
-            $('#yahtzee').addClass("disp-score")
-        }else{
-            $('#yahtzee').removeClass("disp-score")
+            if (arr[i] === "full"){
+                if(!$('#full').hasClass('locked-score')){
+                    $('#full').addClass("disp-score")
+                }
+            }else{
+                $('#full').removeClass("disp-score")
+            }
+            if (arr[i] === "petite suite"){
+                if(!$('#petite-suite').hasClass('locked-score')){
+                    $('#petite-suite').addClass("disp-score")
+                }
+            }else{
+                if(arr[i] !== "grande suite"){
+                    $('#petite-suite').removeClass("disp-score")
+                }
+            }
+            if (arr[i] === "grande suite"){
+                if(!$('#grande-suite').hasClass('locked-score')){
+                    $('#grande-suite').addClass("disp-score")
+                }
+            }else{
+                $('#grande-suite').removeClass("disp-score")
+            }
+            if (arr[i] === "carre"){
+                if(!$('#carre').hasClass('locked-score')){
+                    $('#carre').addClass("disp-score")
+                }
+            }else{
+                $('#carre').removeClass("disp-score")
+            }
+            if (arr[i] === "yahtzee"){
+                if(!$('#yahtzee').hasClass('locked-score')){
+                    $('#yahtzee').addClass("disp-score")
+                }
+            }else{
+                $('#yahtzee').removeClass("disp-score")
+            }
         }
     }
-}
-
-function animateSimple(arr){
-    for(let i = 0; i<arr.length; i++){
-        console.log(arr[i])
-        if(arr[i][1] > 0){
-            $('#score-one').addClass('disp-score')
-        }else{
-            $('#score-one').removeClass('disp-score')
-        }
-        if(arr[i][2] > 0){
-            $('#score-two').addClass('disp-score')
-        }else{
-            $('#score-two').removeClass('disp-score')
-        }
-        if(arr[i][3] > 0){
-            $('#score-three').addClass('disp-score')
-        }else{
-            $('#score-three').removeClass('disp-score')
-        }
-        if(arr[i][4] > 0){
-            $('#score-four').addClass('disp-score')
-        }else{
-            $('#score-four').removeClass('disp-score')
-        }
-        if(arr[i][5] > 0){
-            $('#score-five').addClass('disp-score')
-        }else{
-            $('#score-five').removeClass('disp-score')
-        }
-        if(arr[i][6] > 0){
-            $('#score-six').addClass('disp-score')
-        }else{
-            $('#score-six').removeClass('disp-score')
+    
+    function animateSimple(arr){
+        for(let i = 0; i<arr.length; i++){
+            if(arr[i][1] > 0){
+                if(!$('#score-one').hasClass('locked-score')){
+                    $('#score-one').addClass('disp-score')
+                }
+            }else{
+                $('#score-one').removeClass('disp-score')
+            }
+            if(arr[i][2] > 0){
+                if(!$('#score-two').hasClass('locked-score')){
+                    $('#score-two').addClass('disp-score')
+                }
+            }else{
+                $('#score-two').removeClass('disp-score')
+            }
+            if(arr[i][3] > 0){
+                if(!$('#score-three').hasClass('locked-score')){
+                    $('#score-three').addClass('disp-score')
+                }
+            }else{
+                $('#score-three').removeClass('disp-score')
+            }
+            if(arr[i][4] > 0){
+                if(!$('#score-four').hasClass('locked-score')){
+                    $('#score-four').addClass('disp-score')
+                }
+            }else{
+                $('#score-four').removeClass('disp-score')
+            }
+            if(arr[i][5] > 0){
+                if(!$('#score-five').hasClass('locked-score')){
+                    $('#score-five').addClass('disp-score')
+                }
+            }else{
+                $('#score-five').removeClass('disp-score')
+            }
+            if(arr[i][6] > 0){
+                if(!$('#score-six').hasClass('locked-score')){
+                    $('#score-six').addClass('disp-score')
+                }
+            }else{
+                $('#score-six').removeClass('disp-score')
+            }
         }
     }
-}
+})
